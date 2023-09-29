c09513de5db0d3d8c58b39c9f3b0849d6408e4b3
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-09-29 15:04:09 -0700

    Adds to "sample.js"

--- sample.js
+++ sample.js
@@ -4,4 +4,19 @@ function greet() {
     console.log(MESSAGE);
 }
 
+/**
+ * 
+ * @ztpExport
+ * @param {string} name 
+ * @param {number} count 
+ * @returns {Promise<string>}
+ */
+function doSomethingRemotely(name, count) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Hello, ${name}! You have ${count} new messages.`);
+        }, 1000);
+    });
+}
+
 greet();
